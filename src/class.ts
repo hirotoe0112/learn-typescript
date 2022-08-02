6602fb949a90c5db0cc70bc9c4f97f66504b3bd6
hirotoe0112 <hirotoe0112@gmail.com>
2022-08-03 07:26:34 +0900

    private constructor

--- src/class.ts
+++ src/class.ts
@@ -18,3 +18,18 @@ const mut1 = mut.add(1)
 
 const set = new Set2()
 const set1 = set.add(2)
+
+class MessageQueue{
+  private constructor(private message: string[]){}
+  //インスタンス化を実現するためのメソッド
+  static create(message: string[]){
+    return new MessageQueue(message)
+  }
+}
+
+//private constructorに指定すると拡張不可
+//class BadQueue extends MessageQueue{}
+//newも不可
+//let q = new MessageQueue([])
+//代わりに、createメソッドを実装し、このようにする
+let q = MessageQueue.create([])
