4fc9f84a1aef55503b0c57feb7cf1d7bb44b0f55
hirotoe0112 <hirotoe0112@gmail.com>
2022-08-23 21:29:49 +0900

    matrix

--- src/matrix.ts
+++ src/matrix.ts
@@ -14,20 +14,3 @@ type MatrixProtocol = {
     out: [Matrix]
   }
 }
-
-type Protocol = {
-  [command: string]: {
-    in: unknown[]
-    out: unknown
-  }
-}
-function createProtocol<P extends Protocol>(script: string){
-  return <K extends keyof P>(command: K) =>
-    (...args: P[K]['in']) =>
-      new Promise<P[K]['out']>((resolve, reject) => {
-        let worker = new Worker(script)
-        worker.onerror = reject
-        worker.onmessage = event => resolve(event.data)
-        worker.postMessage({command, args})
-      })
-}
